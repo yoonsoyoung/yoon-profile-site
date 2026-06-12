3e87b0d8b28c173eaa1d3730d812eb3b4a0ed450
yoonsoyoung <hummingsy@gmail.com>
2026-06-12 21:40:26 +0900

    Fix: Update App 내 프로젝트 상세페이지 라우팅 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import { PortfolioPage } from './pages/PortfolioPage';
-import { ProjectDetailPage } from './pages/ProjectDetailPage';
 import './index.css';
 
 function App(): React.ReactElement {
@@ -9,7 +8,6 @@ function App(): React.ReactElement {
     <HashRouter>
       <Routes>
         <Route path="/" element={<PortfolioPage />} />
-        <Route path="/projects/:id" element={<ProjectDetailPage />} />
       </Routes>
     </HashRouter>
   );
